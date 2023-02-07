7c07f486bf821f220d683a79a8c0e932f0d56960
Wo Yong Jeong <woyong.jeong@gmail.com>
2023-02-07 01:31:10 -0500

    BootCampX Queries 1 Exercise

---
+++ 1_queries/1_names_from_single_cohort.sql
@@ -0,0 +1,4 @@
+SELECT id, name, email, cohort_id
+FROM students
+WHERE cohort_id = 1
+ORDER BY name;
---
+++ 1_queries/3_students_without_email_or_phonenumber.sql
@@ -0,0 +1,5 @@
+SELECT name, id
+FROM students
+WHERE email IS NULL
+OR phone IS NULL
+ORDER BY name
